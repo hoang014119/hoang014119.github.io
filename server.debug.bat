e8fe9064379e57cc53d42997085637879041bbe8
Your Name <you@example.com>
2024-08-02 00:26:37 +0700

    server.debug.bat

--- server.debug.bat
+++ server.debug.bat
@@ -1,4 +1,5 @@
 java -jar ../../../server/target/server-0.0.1-SNAPSHOT.jar ^
   --404 ^
-  --replace=test/angular.js:../test/dist/test/angular.debug.js ^
+  --replace=debug:../test/dist/test ^
+  --replace=test/angular.js:debug/runtime.min.js+debug/polyfills.min.js+debug/vendor.min.js+debug/main.min.js ^
   --replace=test/setup.js:../test/src/setup.js
