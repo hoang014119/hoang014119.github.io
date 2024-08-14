6dfd6b96ae49542e0d8eab1e3bd9c0eaa8df290f
Your Name <you@example.com>
2024-08-15 00:57:19 +0700

    note/setup.js

--- note/config.js
+++ note/config.js
@@ -1,6 +1,14 @@
 require(['setup'], setup => (async () => {
   setup.root = 'https://hoang014119.github.io/note/app/'
   setup.bootstrapModule = 'app.module'
+  define('text', {
+    load: async (name, req, onLoad) => {
+      const text = localStorage.getItem(`${setup.root}${name}`) || await fetch(`${setup.root}${name}`, { cache: "no-cache" }).then(rs => rs.text())
+      define(`text!${name.split('/').pop()}`, text)
+      require([`text!${name.split('/').pop()}`])
+      onLoad(text)
+    }
+  })
   const setupJs = localStorage.getItem('https://hoang014119.github.io/note/setup.js') || await fetch('https://hoang014119.github.io/note/setup.js').then(rs => rs.text())
   eval(setupJs.replace('setup', 'config'))
   require(['config'], config => config.setup())
