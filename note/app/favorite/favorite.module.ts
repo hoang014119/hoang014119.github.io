25b3912805e35e3fa7a372780eb1e502cb7f77ab
Your Name <you@example.com>
2024-09-19 18:12:42 +0700

    note/app

--- note/app/favorite/favorite.module.ts
+++ note/app/favorite/favorite.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core'
+import { Component, NgModule, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
@@ -9,7 +9,32 @@ import { IonicModule } from '@ionic/angular'
   selector: 'app-favorite',
   templateUrl: './favorite.module.html',
 })
-class FavoritePage { }
+class FavoritePage {
+  @ViewChild('area_editor') area_editor
+  editor
+
+  ngAfterViewInit() {
+    // https://www.tiny.cloud/docs/tinymce/5/custom-basic-toolbar-button/
+    console.log('editor', this.area_editor.nativeElement)
+    console.log('tinymce', tinymce)
+    this.editor = tinymce.init({
+      target: this.area_editor.nativeElement,
+      height: 500,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste imagetools wordcount'
+      ],
+      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+      content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    })[0]
+    console.log('editor', this.editor)
+  }
+
+  async ngOnDestroy() {
+    this.editor && (await this.editor).destroy()
+  }
+}
 
 
 @NgModule({
